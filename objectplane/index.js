0f5c4568f7fb624e01717c8fbf7339148f0f61ec
cublegend <cubayf@gmail.com>
2021-06-30 15:40:21 +0800

    masking sample

--- objectplane/index.js
+++ objectplane/index.js
@@ -1,1 +1,13 @@
-
+AFRAME.registerComponent('cloak-obj', {
+    init: function() {
+        let mesh = this.el.getObject3D('mesh');
+            if (mesh == undefined) return;
+            mesh.traverse(function(node) {
+                if (node.isMesh && node.material) {
+                    console.log(node);
+                    node.material.colorWrite = false;
+                    node.material.needsUpdate = true;
+                }
+            });
+    }
+})
