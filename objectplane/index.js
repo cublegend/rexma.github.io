935de343bc5b740684a9cf80aec013e2877334a0
cublegend <cubayf@gmail.com>
2021-06-30 17:32:32 +0800

    object place initial commit

--- objectplane/index.js
+++ objectplane/index.js
@@ -1,13 +1,19 @@
-AFRAME.registerComponent('cloak-obj', {
+AFRAME.registerComponent('cube', {
     init: function() {
-        let mesh = this.el.getObject3D('mesh');
-            if (mesh == undefined) return;
-            mesh.traverse(function(node) {
-                if (node.isMesh && node.material) {
-                    console.log(node);
-                    node.material.colorWrite = false;
-                    node.material.needsUpdate = true;
-                }
-            });
+        document.addEventListener('click', this._Get_position.bind(true));
+        
+    },
+
+    _Get_position: function(event) {
+        let markers = document.querySelectorAll('a-nft');
+        let positions = [];
+        for (let i = 0; i < markers.length; i++) {
+            positions[i] = markers[i].object3D.position;
+            console.log(markers[i].object3D.position);
+        }
+        let cloak = document.createElement('a-box');
+        let width = (positions[0].x - positions[1].x) * 2;
+        cloak.setAttribute("position", {x: positions[0].x, y: positions[1].y, z: positions[2].z});
+        
     }
 })
