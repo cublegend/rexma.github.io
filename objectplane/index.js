d675816e1591e890f9fb45e9065200deddb39695
cublegend <cubayf@gmail.com>
2021-07-01 14:30:52 +0800

    click colorful floor

--- objectplane/index.js
+++ objectplane/index.js
@@ -1,19 +1,61 @@
-AFRAME.registerComponent('cube', {
-    init: function() {
-        document.addEventListener('click', this._Get_position.bind(true));
-        
+AFRAME.registerComponent('building-floor', {
+    schema: {
+        height: {type: 'float', default: 0.3},
+        width: {type: 'float', default: 1},
+        color: {type: 'color', default: '#C06C84'},
+        random_color: {type: 'boolean', default: true}
     },
 
-    _Get_position: function(event) {
-        let markers = document.querySelectorAll('a-nft');
-        let positions = [];
-        for (let i = 0; i < markers.length; i++) {
-            positions[i] = markers[i].object3D.position;
-            console.log(markers[i].object3D.position);
+    init: function() {
+        let floor = this.el;
+        let data = this.data;
+        floor.setAttribute("height", data.height);
+        floor.setAttribute("width", data.width);
+        if (this.data.random_color) {
+            floor.setAttribute("color", this._random_color_gen());
         }
-        let cloak = document.createElement('a-box');
-        let width = (positions[0].x - positions[1].x) * 2;
-        cloak.setAttribute("position", {x: positions[0].x, y: positions[1].y, z: positions[2].z});
-        
+        else {
+            floor.setAttribute("color", data.color);
+        }
+    },
+
+    _random_color_gen: function() {
+        var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+              color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+    }
+})
+
+AFRAME.registerComponent('scene-event-handler', {
+    on_marker: null,
+
+    init: function() {
+        document.addEventListener('markerFound', function() {
+            this.on_marker = true;
+        }.bind(this));
+        document.addEventListener('markerLost', function() {
+            this.on_marker = false;
+        }.bind(this));
+
+        this.el.addEventListener("click", this._instantiate_floor.bind(this));
+    },
+
+    _instantiate_floor: function(event) {
+        if (!this.on_marker) {
+            return;
+        }
+        if (event.detail.intersection == undefined) return;
+        let point = document.querySelector('a-marker').object3D.worldToLocal(event.detail.intersection.point); //HERE!!!
+        console.log("instantiate floor at mouse position at: " + event.detail.intersection.point.z);
+        let new_floor = document.createElement('a-box');
+        new_floor.setAttribute('building-floor', '');
+
+        document.querySelector('a-marker').appendChild(new_floor);
+        new_floor.object3D.position.x = point.x;
+        new_floor.object3D.position.y = point.y;
+        new_floor.object3D.position.z = point.z;
     }
 })
