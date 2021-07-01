912f7fe790d9bd8dcb19ceb7dab80a9ee7018e14
cublegend <cubayf@gmail.com>
2021-07-01 18:00:13 +0800

    click colorful floor

--- objectplane/index.js
+++ objectplane/index.js
@@ -41,21 +41,7 @@ AFRAME.registerComponent('scene-event-handler', {
         document.addEventListener('markerLost', function() {
             this.on_marker = false;
         }.bind(this));
-
-        document.addEventListener('markerFound', this._create_click_plane.bind(this));
-
-        document.querySelector('#base').addEventListener("click", this._instantiate_floor.bind(this));
-    },
-
-    _create_click_plane: function(event) {
-        if (this.plane != null) return;
-        let p = document.createElement('a-plane');
-        this.plane = p;
-        document.querySelector('a-marker').appendChild(p);
-        p.setAttribute('rotation', {x: -90, y: 0, z: 0});
-        p.setAttribute('color', 'beige');
-        p.object3D.scale.set(3, 3, 3);
-        p.addEventListener('click', this._instantiate_floor.bind(this));
+        document.querySelector('a-marker').addEventListener("click", this._instantiate_floor.bind(this));
     },
 
     _instantiate_floor: function(event) {
@@ -63,14 +49,13 @@ AFRAME.registerComponent('scene-event-handler', {
             return;
         }
         if (event.detail.intersection == undefined) return;
-        let point = document.querySelector('a-marker').object3D.worldToLocal(event.detail.intersection.point); //HERE!!!
-        console.log("instantiate floor at mouse position at: " + event.detail.intersection.point.z);
+        let point = document.querySelector('a-marker').object3D.worldToLocal(event.detail.intersection.point); 
+        console.log(event.detail.intersection.object.parent.parent);
         let new_floor = document.createElement('a-box');
         new_floor.setAttribute('building-floor', '');
 
         document.querySelector('a-marker').appendChild(new_floor);
         new_floor.object3D.position.x = point.x;
-        new_floor.object3D.position.y = point.y;
         new_floor.object3D.position.z = point.z;
     }
 })
