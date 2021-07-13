556f23d4496e06c07222e54e3b775f4b91dde8c8
cublegend <cubayf@gmail.com>
2021-07-13 18:05:43 +0800

    added animation on portfolio page

---
+++ cube_manager.js
@@ -0,0 +1,86 @@
+AFRAME.registerComponent('cube-manager', {
+    init: function () {
+        // create random cubes that follow mouse position parallex movement
+
+        // how many cubes
+        let num_cubes = Math.random() * (50, 30) + 30;
+        for (let i = 0; i < num_cubes; i++) {
+            this._create_cubes();
+        }
+        this.el.addEventListener('mousemove', this._mouse_to_world.bind(this));
+    },
+
+    _create_cubes: function() {
+        // randomize cube setup
+        let canvas = document.querySelector('canvas');
+        let rect = document.querySelector('a-scene').getBoundingClientRect();
+        let width_max = ( (canvas.width - rect.left) / rect.width ) * 2 - 1;
+        let width_min = ( (0 - rect.left) / rect.width ) * 2 - 1;
+        let height_max = - ( (canvas.height - rect.top) / rect.height ) * 2 + 1;;
+        let height_min = - ( (0 - rect.top) / rect.height ) * 2 + 1;;
+        let pos_x = Math.random() * (width_max - width_min) + width_min;
+        let pos_y = Math.random() * (height_max - height_min) + height_min;
+        let pos_z = Math.random() * -3;
+        let rotation_x = Math.random() * 8;
+        let rotation_y = Math.random() * 8;
+        let rotation_z = Math.random() * 8;
+        let n = Math.random() * (10000 - 3000) + 3000;
+
+        // create cube
+        let new_cube = document.createElement('a-box');
+        new_cube.object3D.position.set(pos_x, pos_y, pos_z);
+        new_cube.object3D.scale.set(0.05, 0.05, 0.05);
+        new_cube.object3D.rotation.set(rotation_x, rotation_y, rotation_z);
+        new_cube.setAttribute('color', 'grey');
+        new_cube.setAttribute('material', 'opacity', '0');
+        
+        
+        // set animations
+        new_cube.setAttribute('animation__enter',{
+            property: 'material.opacity',
+            to: '1',
+            dur: n / 2
+        });
+        new_cube.setAttribute('animation__rotation',{
+            property: 'rotation',
+            to: '30 60 0',
+            dur: n,
+            loop: 'true',
+            easing: 'linear'
+        });
+       
+        this.el.appendChild(new_cube);
+
+        // remove when animation complete
+        new_cube.addEventListener('animationcomplete', (e) => {
+            if (e.detail.name == 'animation__enter') {
+                new_cube.setAttribute('animation__opacity',{
+                    property: 'material.opacity',
+                    to: '0',
+                    dur: n / 2,
+                    easing: 'linear'
+                });
+                return;
+            }
+            new_cube.parentNode.removeChild(new_cube);
+            // create a new cube whenever one is gone
+            this._create_cubes();
+        });
+    },
+
+    _mouse_to_world: function(e) {
+        let mouse = new THREE.Vector2();
+        let camera = AFRAME.scenes[0].camera;
+        let rect = document.querySelector('a-scene').getBoundingClientRect();
+        mouse.x = ( (e.clientX - rect.left) / rect.width ) * 2 - 1;
+        mouse.y = - ( (e.clientY - rect.top) / rect.height ) * 2 + 1;
+        let vector = new THREE.Vector3( mouse.x, mouse.y, -1 ).unproject( camera );
+        let rot = new THREE.Vector3();
+        let ratio = 2;
+        rot.y = -vector.x * 360 * ratio;
+        rot.x = vector.y * 360 * ratio;
+        rot.z = vector.z * 360 * ratio;
+        document.querySelector('[camera]').setAttribute('rotation',{x: rot.x, y: rot.y, z: rot.z} );
+
+    }
+})
--- portfolio_menu.js
+++ portfolio_menu.js
@@ -1,5 +1,12 @@
+var t;
+var l;
+
 $(document).ready(function() {
+    t = $('.circle').css("top");
+    l = $('.circle').css("left");
+
     $("#num1").on('click', () => {
+        $('.circle').css({'animation': 'shadow 1s forwards'});
         $('.AR').css("display", "block");
         $("#num2").animate({
             left: "+=100%",
@@ -15,6 +22,7 @@ $(document).ready(function() {
                 left: "+=5%",
                 top: "+=10%"
             }, "slow", () => {
+                $('a-scene').css('display', 'none');
                 $("#num1").animate({
                     left: "-=50%",
                     top: "-=100%"
@@ -25,7 +33,7 @@ $(document).ready(function() {
                     top: "-=250vh",
                     left: "-=250vh"
                 }, 800, () => {
-                    $('.circle').css("z-index", "-10"); // set the z index so that i can click on other things
+                    $('.circle').css("display", "none");
                     $(".button-cover").animate({
                         borderRadius: "50%"
                     }, "slow")
@@ -33,12 +41,14 @@ $(document).ready(function() {
                 $(".AR").animate({
                     paddingTop: "-=10%",
                     opacity:"1"
-                }, 600);
+                }, 300);
             });
             
         }, 200);
     });
     $("#num2").on('click', () => {
+        $('.circle').css({'animation': 'shadow 1s forwards'});
+        $('.Game').css("display", "block");
         $("#num1").animate({
             left: "-=50%",
             top:"-=100%",
@@ -49,8 +59,37 @@ $(document).ready(function() {
             top: "+=100%",
             opacity: 0
         }, "slow");
+        
+        setTimeout(() => {
+            $("#num2").animate({
+                left: "-=5%"
+            }, "slow", () => {
+                $('a-scene').css('display', 'none');
+                $("#num2").animate({
+                    left: "+=100%"
+                }, 800);
+                $(".circle").animate({
+                    width: "+=500vh",
+                    height: "+=500vh",
+                    top: "-=250vh",
+                    left: "-=250vh"
+                }, 800, () => {
+                    $('.circle').css("display", "none");
+                    $(".button-cover").animate({
+                        borderRadius: "50%"
+                    }, "slow")
+                });
+                $(".Game").animate({
+                    paddingTop: "-=10%",
+                    opacity:"1"
+                }, 300);
+            });
+            
+        }, 200);
     });
     $("#num3").on('click', () => {
+        $('.circle').css({'animation': 'shadow 1s forwards'});
+        $('.Music').css("display", "block");
         $("#num2").animate({
             left: "+=100%",
             opacity: 0
@@ -60,9 +99,71 @@ $(document).ready(function() {
             top: "-=100%",
             opacity: 0
         }, "slow");
+        
+        setTimeout(() => {
+            $("#num3").animate({
+                left: "+=5%",
+                top: "-=10%"
+            }, "slow", () => {
+                $('a-scene').css('display', 'none');
+                $("#num3").animate({
+                    left: "-=50%",
+                    top: "+=100%"
+                }, 600);
+                $(".circle").animate({
+                    width: "+=500vh",
+                    height: "+=500vh",
+                    top: "-=250vh",
+                    left: "-=250vh"
+                }, 800, () => {
+                    $('.circle').css("display", "none");
+                    $(".button-cover").animate({
+                        borderRadius: "50%"
+                    }, "slow")
+                });
+                $(".Music").animate({
+                    paddingTop: "-=10%",
+                    opacity:"1"
+                }, 300);
+            });
+            
+        }, 200);
     });
 })
 
 function back_to_menu() {
     console.log("we're back!");
+    $("#num1").animate({
+        left: "50%",
+        top: "50%",
+        opacity: 1
+    }, "slow");
+    $("#num2").animate({
+        left: "50%",
+        top: "50%",
+        opacity: 1
+    }, "slow");
+    $("#num3").animate({
+        left: "50%",
+        top: "50%",
+        opacity: 1
+    }, "slow");
+    $('.circle').css("display", "inline-block");
+    $(".circle").animate({
+        width: "30vh",
+        height: "30vh",
+        top: t,
+        left: l
+    }, 800, () => {
+        $('a-scene').css('display', 'block');
+        $(".button-cover").css("border-radius", '0');
+        $(".AR").css("display", "none");
+        $(".Game").css("display", "none");
+        $(".Music").css("display", "none");
+
+        $(".AR").css("opacity", "0");
+        $(".Game").css("opacity", "0");
+        $(".Music").css("opacity", "0");
+        $('.circle').css({'animation': 'shadow-back 0.3s forwards'});
+    });
 }
