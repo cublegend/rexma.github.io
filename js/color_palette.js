a4ab344bf7071aa0e42e1f35950f051b1fdc680b
cublegend <cubayf@gmail.com>
2024-01-11 15:44:27 -0500

    added color palette support

--- js/color_palette.js
+++ js/color_palette.js
@@ -14,22 +14,6 @@ $(window).scroll(function () {
 });
 
 function changeColorPaletteForSection($section) {
-    // Assuming different color palettes are determined by section ID or another attribute
-    var sectionId = $section.attr('id');
-
-    switch (sectionId) {
-        case 'section1':
-            changeColorPalette('#FFFFFF', '#000000'); // Example: white background, black text
-            break;
-        case 'section2':
-            changeColorPalette('#000000', '#FFFFFF'); // Example: black background, white text
-            break;
-        // Add more cases as needed
-    }
-}
-
-function changeColorPalette(backgroundColor, textColor) {
-    $('body').css('background-color', backgroundColor);
-    $('.curved-text').find('span').css('color', textColor);
-    $('.title-section').css('color', textColor);
+    var palette = $section.data('palette');
+    document.documentElement.className = palette;
 }
