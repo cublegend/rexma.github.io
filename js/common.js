d8e29fc9e86bf4cbeb7969cbddd2f5330b72a521
cublegend <cubayf@gmail.com>
2024-02-06 13:30:43 -0500

    completed three buttons

--- js/common.js
+++ js/common.js
@@ -1,3 +1,28 @@
+// list of pages
+var pages = ['music', 'game', 'photo'];
+var currentPage = 0;
+
 $(window).on('load', function () {
     $('#transition-screen').delay(200).fadeOut(10);
+    var pageName = window.location.pathname.split('/').pop().split('.')[0];
+    currentPage = pages.indexOf(pageName);
+});
+
+$('#left-button').on('click', function () {
+    // clockwise rotate between pages
+    currentPage = (currentPage + 1) % pages.length;
+    // change page
+    window.location.href = pages[currentPage] + '.html';
+});
+
+$('#right-button').on('click', function () {
+    // counter-clockwise rotate between pages
+    currentPage = (currentPage - 1 + pages.length) % pages.length;
+    // change page
+    window.location.href = pages[currentPage] + '.html';
+});
+
+$('#close-button').on('click', function () {
+    // close the current page
+    window.location.href = 'index.html';
 });
