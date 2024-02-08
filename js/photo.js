c0bfeff2df6dfcc54cfef84444a4edefe43d9526
cublegend <cubayf@gmail.com>
2024-02-08 14:34:25 -0500

    draft complete

--- js/photo.js
+++ js/photo.js
@@ -23,26 +23,6 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    function positionTextElementRandomly(element) {
-        const randomY = Math.random() * 100;
-        const randomX = Math.random() * 100;
-        element.css('top', `${randomY}%`);
-        element.css('left', `${randomX}%`);
-    }
-    $('.text-container').each(function () {
-        const textToSpread = $(this).attr('data-text');
-        const numberOfElements = 25;
-        let fontSize = $(this).attr('data-font-size') || 20;
-        let blur = $(this).attr('data-blur') || 0;
-        $(this).css('font-size', `${fontSize}em`);
-        $(this).css('filter', `blur(${blur}px)`);
-        for (let i = 0; i < numberOfElements; i++) {
-            const textElement = $('<span class="text-element"></span>');
-            textElement.text(textToSpread);
-            $(this).append(textElement);
-            positionTextElementRandomly(textElement);
-        }
-    });
     $('.foreground-content img').each(function () {
         $(this).css('top', `${Math.random() * 30 + 25}%`);
         $(this).css('left', `${Math.random() * 50 + 10}%`);
