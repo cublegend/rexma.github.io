9794d21f73a16c7edc5f896b50aeb98f934b447a
cublegend <cubayf@gmail.com>
2024-02-13 01:13:12 -0500

    Edit scrolling effect for music for better performance on different browsers
    Also changed the scrolling for the photo section

--- js/music.js
+++ js/music.js
@@ -1,65 +1,36 @@
 $(document).ready(function () {
-    var sectionHeight = $('.scroll-content').height(); // assume all same height
-    $('.scroll-content').each(function () {
-        var $this = $(this);
-        var sectionStart = $this.offset().top;
-        var sectionEnd = sectionStart + sectionHeight;
-        $('.scroll-container').on('scroll', function () {
-            var scrollPosition = $('.scroll-container').scrollTop() + $('.scroll-container').offset().top;
-            if (scrollPosition >= sectionStart - sectionHeight &&
-                scrollPosition <= sectionStart) {
-                var scrollAmount = scrollPosition - sectionStart;
-                $this.find('h1').css({
-                    'opacity': 1 + scrollAmount / sectionHeight,
-                    'filter': 'blur(' + (-scrollAmount / sectionHeight) * 10 + 'px)'
+    $('.menu').scroll(function () {
+        $('.title').each(function (idx, title) {
+            var titleMid = $(this).offset().top + $(this).height() / 2;
+            var backgroundVideo = $('.background-video').eq(idx);
+            var top = $('.menu').offset().top;
+            var center = $('.menu').height() / 2 + top;
+            if (titleMid <= center && titleMid >= top) {
+                var opacity = 1 - (center - titleMid) / (center - top);
+                backgroundVideo.css('opacity', opacity);
+            } else if (titleMid > center && titleMid <= top + $('.menu').height()) {
+                var opacity = 1 - (titleMid - center) / (top + $('.menu').height() - center);
+                backgroundVideo.css('opacity', opacity);
+            }
+            if (backgroundVideo.css('opacity') < 0.3) {
+                backgroundVideo.get(0).pause();
+                backgroundVideo.css({
+                    'opacity': 0,
+                    'pointer-events': 'none'
                 });
-            } else if (scrollPosition > sectionStart &&
-                scrollPosition <= sectionEnd) {
-                var scrollAmount = scrollPosition - sectionStart;
-                $this.find('h1').css({
-                    'opacity': 1 - scrollAmount / sectionHeight,
-                    'filter': 'blur(' + scrollAmount / sectionHeight * 10 + 'px)'
+            } else if (backgroundVideo.css('opacity') > 0.8) {
+                backgroundVideo.get(0).play();
+                backgroundVideo.css({
+                    'opacity': 1,
+                    'pointer-events': 'auto'
                 });
             }
 
-            // video transitions
-            var video = $this.find('.background-video');
-            if (scrollPosition >= sectionStart - sectionHeight
-                && scrollPosition <= sectionStart) {
-                video.css('opacity', 1 + scrollAmount / sectionHeight * 2);
-
-            } else if (scrollPosition > sectionStart &&
-                scrollPosition <= sectionEnd) {
-                var scrollAmount = scrollPosition - sectionStart;
-                video.css('opacity', 1 - scrollAmount / sectionHeight * 2);
-            }
-
-            // video pointer-events control
-            // play video when in view
-            if (scrollPosition >= sectionStart - 50
-                && scrollPosition <= sectionEnd) {
-                video.css('pointer-events', 'auto');
-                video[0].play();
-            } else {
-                video.css('pointer-events', 'none');
-                video[0].pause();
-            }
         });
     });
 
-    $('.scroll-content').first().find('.background-video').css({
-        'opacity': 1,
-        'pointer-events': 'auto'
-    });
-    $('.scroll-content').first().find('.background-video')[0].play();
-
-    // when fullscreen video events are detected, set opacity to 1
-    $('.background-video').each(function () {
-        var $this = $(this);
-        $this.on('fullscreenchange', function () {
-            $this.css('opacity', 1);
-        });
-    });
+    $('.background-video').first().css('opacity', 1);
+    $('.background-video').first().get(0).play();
 
 });
 
